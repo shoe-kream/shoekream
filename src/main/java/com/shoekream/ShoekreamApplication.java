1983d435259a62e07ca6be1436fd00255d448da0
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-03-28 00:26:23 +0900

    [build] : project 생성

---
+++ src/main/java/com/shoekream/ShoekreamApplication.java
@@ -0,0 +1,13 @@
+package com.shoekream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoekreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoekreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoekream/ShoekreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoekream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoekreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
