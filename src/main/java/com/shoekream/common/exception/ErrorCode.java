48d7ca65eb51782d62716756545b241d0eef3dc3
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-03-31 00:25:13 +0900

    [feat] : 브랜드 조회, 리스트 조회, 등록 에러 코드 추가

--- src/main/java/com/shoekream/common/exception/ErrorCode.java
+++ src/main/java/com/shoekream/common/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "가입된 회원을 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "가입된 회원을 찾을 수 없습니다."),
+    BRAND_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 브랜드를 찾을 수 없습니다."),
+    DUPLICATED_BRAND(HttpStatus.CONFLICT, "이미 등록되어 있는 브랜드입니다.");
 
     private HttpStatus httpStatus;
     private String message;
