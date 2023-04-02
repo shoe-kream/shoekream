85a481cea3390b477731fad8842aebb85d233bf3
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-04-03 01:38:00 +0900

    [refactor] : BindingResult aop 적용

--- src/main/java/com/shoekream/controller/UserApiController.java
+++ src/main/java/com/shoekream/controller/UserApiController.java
@@ -1,40 +1,34 @@
 package com.shoekream.controller;
 
 import com.shoekream.common.Response;
-import com.shoekream.common.exception.BindingException;
-import com.shoekream.common.exception.ErrorCode;
-import com.shoekream.common.exception.ShoeKreamException;
 import com.shoekream.domain.user.dto.UserCreateRequest;
 import com.shoekream.domain.user.dto.UserCreateResponse;
 import com.shoekream.domain.user.dto.UserLoginRequest;
 import com.shoekream.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserApiController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<Response<UserCreateResponse>> create(@Validated @RequestBody UserCreateRequest request, BindingResult br) {
-
+    public ResponseEntity<Response<UserCreateResponse>> create(@Validated @RequestBody UserCreateRequest request) {
         UserCreateResponse response = userService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(Response.success(response));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Response<String>> login(@Validated @RequestBody UserLoginRequest request, BindingResult br) {
-
+    public ResponseEntity<Response<String>> login(@Validated @RequestBody UserLoginRequest request) {
         String jwt = userService.loginUser(request);
         return ResponseEntity.ok(Response.success(jwt));
     }
-
 }
