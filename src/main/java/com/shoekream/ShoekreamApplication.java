3b880ea5faeefe80b2063b5f926b0d750d717601
percykwon <85394884+percyfrank@users.noreply.github.com>
2023-04-07 01:07:14 +0900

    [feat] : 캐싱 기능 활성화

--- src/main/java/com/shoekream/ShoekreamApplication.java
+++ src/main/java/com/shoekream/ShoekreamApplication.java
@@ -2,8 +2,10 @@ package com.shoekream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ShoekreamApplication {
 
 	public static void main(String[] args) {
